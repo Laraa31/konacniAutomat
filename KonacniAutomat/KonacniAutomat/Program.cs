ee8203a9b5384f95ada325a8c2778da84aeac083
Laraa31 <88662021+Laraa31@users.noreply.github.com>
2022-02-11 11:09:49 +0100

    Add files via upload

---
+++ KonacniAutomat/KonacniAutomat/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KonacniAutomat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            int y=0;
+           
+            while (y==0)
+            {
+                Console.WriteLine("Upiši bit: ");
+                if (Convert.ToInt32(Console.ReadLine()) == 1)
+                {
+                    Console.WriteLine("Upiši bit: ");
+                    if (Convert.ToInt32(Console.ReadLine()) == 1)
+                    {
+                        Console.WriteLine("Upiši bit: ");
+                        if (Convert.ToInt32(Console.ReadLine()) == 0)
+                        {
+                            Console.WriteLine("Upiši bit: ");
+                            if (Convert.ToInt32(Console.ReadLine()) == 1)
+                            {
+                                Console.WriteLine("Diži rampu");
+                                y++;
+                            }
+                        }
+                    }
+                }
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
